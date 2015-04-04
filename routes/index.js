45f90653d0875209a9c670a2338d37c7cf0541b4
李茂峰 <limaofeng@msn.com>
2015-04-05 02:22:03 +0800

    关于我们取动态内容。公司页面内容取接口数据

--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,14 @@ router.get('/designers/:id', function (req, res) {
     });
 });
 router.get('/about', function (req, res) {
-    res.render('about', {menus: {about: true}, partials: {header: 'header', footer: 'footer'}});
+    http.get({
+        path: '/cms/articles?EQS_category.code=about'
+    }, function (_res) {
+        _res.on('complete',function (body) {
+            res.render('about', {menus: {about: true},content:body.pageItems[0].content, partials: {header: 'header', footer: 'footer'}});
+        });
+    });
+
 });
 router.get('/furniture', function (req, res) {
    var date =  {menus: {furniture: true},products:[
@@ -65,22 +72,14 @@ router.get('/furniture/:id', function (req, res) {
     res.render('furniture/details', {menus: {furniture: true}, partials: {header: 'header',footer: 'footer'}});
 });
 
-
-router.get('/repair', function (req, res) {
-    res.render('repair', {menus: {repair: true},repairs:[
-        {name:"上海臻逸建筑装饰工程有限公司",url:"http://9041188.czvv.com/",pic:'/images/coll_2.png'},
-        {name:"申远空间设计",url:"http://www.sy-021.com/",pic:'/images/coll_2.png'},
-        {name:"统帅装饰",url:"http://www.tszh.net/",pic:'/images/coll_2.png'},
-        {name:"上海臻茂建筑装饰工程有限公司",url:"http://sh.to8to.com/zs/company972569/",pic:'/images/coll_2.png'},
-        {name:"上海龙发建筑装饰工程有限公司",url:"http://sh.xtuan.com/35621/",pic:'/images/coll_2.png'},
-        {name:"上海雅仕居装饰集团",url:"http://www.sh-ysjdec.com/",pic:'/images/coll_2.png'},
-        {name:"上海石澜装饰设计",url:"http://www.shejiben.com/sjs/1513150/",pic:'/images/coll_2.png'},
-        {name:"上海显尚装饰工程设计有限公司",url:"http://sh.tobosu.com/member/24891/",pic:'/images/coll_2.png'},
-        {name:"同济装潢设计",url:"http://www.shtjzh.com/",pic:'/images/coll_2.png'},
-        {name:"上海嘉墅建筑装饰工程有限公司",url:"http://shzx.cxzg.com/",pic:'/images/coll_2.png'},
-        {name:"圣泓国际",url:"http://www.shshenghong.cn/",pic:'/images/coll_2.png'},
-        {name:"波涛装饰集团",url:"http://www.shbotao.net/",pic:'/images/coll_2.png'}
-    ], partials: {header: 'header', footer: 'footer'}});
+router.get('/companys', function (req, res) {
+    http.get({
+        path: '/cms/articles?EQS_category.code=company'
+    }, function (_res) {
+        _res.on('complete',function (body) {
+            res.render('companys', {menus: {repair: true},companys:body.pageItems, partials: {header: 'header', footer: 'footer'}});
+        });
+    });
 });
 
 router.get('/news', function (req, res) {
